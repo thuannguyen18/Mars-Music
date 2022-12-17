cbef8eee572b506b138116f9f45b4616d1185b14
DzuyKhanh978 <2151013014duy@ou.edu.vn>
2022-12-17 21:57:07 +0700

    Project

--- js/signup.js
+++ js/signup.js
@@ -4,6 +4,7 @@ const inputPasswordRegister = document.querySelector(".input-signup-password");
 const btnRegister = document.querySelector(".signup__signInButton");
 const checkbox1 = document.getElementById('checkbox1')
 const checkbox2 = document.getElementById('checkbox2')
+const inputEmailRegister = document.querySelector(".input-signup-email");
 
 
 // validation form register and register user local storage
@@ -19,9 +20,11 @@ btnRegister.addEventListener("click", (e) => {
       alert("Vui lòng chấp nhận điều khoản")
     } else {
     // array user
+    console.log(inputEmailRegister.value)
     const user = {
       username: inputUsernameRegister.value,
       password: inputPasswordRegister.value,
+      email: inputEmailRegister.value,
     };
     let json = JSON.stringify(user);
     localStorage.setItem('account', json);
