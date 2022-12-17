07246a39b13aafc10d9f0ad6f5d7697201369143
DzuyKhanh978 <2151013014duy@ou.edu.vn>
2022-12-17 20:56:33 +0700

    project

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ btnLogin.addEventListener("click", (e) => {
       user.password === inputPassword.value
     ) {
       alert("Đăng Nhập Thành Công");
-      window.location.href = "index.html";
+      window.location.href = "music-player.html";
     } else  {
       alert("Đăng Nhập Thất Bại");
     }
